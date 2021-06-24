bd7046bbce005b8489995c5e00478108a5b9bfe2
longwu <wu.long@liangyihui.net>
2021-06-24 11:29:59 +0800

    feat: 增加配置

---
+++ index.js
@@ -0,0 +1,31 @@
+module.exports = {
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': [
+      'error',
+      { multiline: { allowFirstLine: false } }
+    ],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        singleline: 'never',
+        multiline: 'never'
+      }
+    ],
+    'vue/space-infix-ops': ['error'],
+    'space-before-function-paren': ['error', 'never'],
+    'vue/mustache-interpolation-spacing': ['error']
+  }
+}
